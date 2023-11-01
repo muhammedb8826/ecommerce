6fd9652e603a86f5fa3f19661a7045b3193b7d70
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-11-01 08:23:32 +0300

    Add search toggle button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,30 @@
 import SearchButton from './SearchButton';
 import SeachForm from './SeachForm';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 const Home = () => {
   const [showSearchBox, setShowSearchBox] = useState(true);
+  const [toggleSearchButton, setToggleSearchButton] = useState(true);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setToggleSearchButton(false);
+        setShowSearchBox(true);
+      } else {
+        setToggleSearchButton(true);
+        setShowSearchBox(false);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   const handleClick = () => {
     setShowSearchBox(!showSearchBox);
   };
   return (
     <section className="home">
-      <SearchButton handleClick={handleClick} showSearchBox={showSearchBox} />
+      {toggleSearchButton && (
+        <SearchButton handleClick={handleClick} showSearchBox={showSearchBox} />
+      )}
       {showSearchBox && <SeachForm />}
     </section>
   );
