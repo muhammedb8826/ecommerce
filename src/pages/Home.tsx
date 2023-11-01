5d5c01e7fc4bdc5d245ca2ed217adaae82a5cb9f
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-11-01 18:50:07 +0300

    Fix toggle form on button click

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,16 @@
 import SearchButton from './SearchButton';
 import SeachForm from './SeachForm';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 const Home = () => {
-  const [showSearchBox, setShowSearchBox] = useState(true);
-  const [toggleSearchButton, setToggleSearchButton] = useState(true);
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setToggleSearchButton(false);
-        setShowSearchBox(true);
-      } else {
-        setToggleSearchButton(true);
-        setShowSearchBox(false);
-      }
-    };
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  const [showSearchBox, setShowSearchBox] = useState(false);
   const handleClick = () => {
     setShowSearchBox(!showSearchBox);
   };
+
   return (
     <section className="home">
-      {toggleSearchButton && (
-        <SearchButton handleClick={handleClick} showSearchBox={showSearchBox} />
-      )}
-      {showSearchBox && <SeachForm />}
+      <SearchButton handleClick={handleClick} showSearchBox={showSearchBox} />
+      <SeachForm showSearchBox={showSearchBox} />
     </section>
   );
 };
