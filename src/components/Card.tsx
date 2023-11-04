3cf54fafd311be7e283cf30452b2240d900dbb71
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-11-04 18:32:02 +0300

    Handle the favorite button

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,16 +4,36 @@ import { SlLocationPin } from 'react-icons/sl';
 import { IoIosArrowRoundForward } from 'react-icons/io';
 import { CardData } from '../utils/CardData';
 import { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+
+type Favorite = {
+  id: string | number;
+};
 
 const Card = () => {
   const [expanded, setExpanded] = useState(4);
+  const [favorites, setFavorites] = useState<Favorite[]>([]);
 
   const handleExpand = () => {
     setExpanded(expanded + 4);
   };
 
-  console.log(expanded);
-  console.log(CardData.length);
+  const handleFavorite = (cardId: string | number, e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const newFavorites = [...favorites];
+    const cardIndex = newFavorites.findIndex(
+      (favorite) => favorite.id === cardId
+    );
+
+    if (cardIndex !== -1) {
+      newFavorites.splice(cardIndex, 1);
+    } else {
+      newFavorites.push({ id: cardId });
+    }
+
+    setFavorites(newFavorites);
+  };
 
   const slicedData = CardData.slice(0, expanded);
 
@@ -22,10 +42,18 @@ const Card = () => {
       <h2>Top Cars</h2>
       <div className="card-container">
         {slicedData.map((card) => (
-          <NavLink to="#" className="card" key={card.id}>
+          <NavLink to={`cars/${card.id}`} className="card" key={card.id}>
             <div className="card-image">
               <img src={card.image} alt="" />
-              <span>{card.favorite}</span>
+              <span onClick={(event) => handleFavorite(card.id, event)}>
+                <FaStar
+                  className={
+                    favorites.some((favorite) => favorite.id === card.id)
+                      ? 'favorite'
+                      : 'unfavorite'
+                  }
+                />
+              </span>
             </div>
             <div className="card-content">
               <div className="card-header">
